d0d3c2755a47e34543a17dd1b9366235be5458fc
Yanislav Igonin <yanislavdabest@gmail.com>
2021-07-19 23:41:15 +0300

    print deck

--- 2. cards/main.go
+++ 2. cards/main.go
@@ -1,14 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
-
-	for i, card := range cards {
-		fmt.Println(card)
-	}
+	cards.print()
 }
 
 func newCard() string {
