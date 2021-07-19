5717175e94321f0e6edd2e248bb2a42f8daf1f59
Yanislav Igonin <yanislavdabest@gmail.com>
2021-07-20 00:06:25 +0300

    feat: newDeck

--- 2. cards/deck.go
+++ 2. cards/deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // which is slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Heart", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
