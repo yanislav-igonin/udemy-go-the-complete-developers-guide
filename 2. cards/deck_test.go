582085e7213fcbde7730bc96a563d05fddfdcda5
Yanislav Igonin <yanislavdabest@gmail.com>
2021-07-27 14:36:50 +0300

    feat: add test

---
+++ 2. cards/deck_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	var filename = "_decktestings"
+	os.Remove(filename)
+
+	d := newDeck()
+	d.saveToFile(filename)
+
+}
